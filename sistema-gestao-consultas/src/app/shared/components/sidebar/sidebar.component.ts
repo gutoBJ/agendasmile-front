3c4e47bed468db9438df50359b68f4ee979cefb2
Augusto <98566110+gutoBJ@users.noreply.github.com>
2026-06-10 16:40:13 -0300

    feat: adicionar layout base com sidebar e header e correção do styles.scss

---
+++ sistema-gestao-consultas/src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, MatIconModule, CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+  constructor(public auth: AuthService) {}
+}
