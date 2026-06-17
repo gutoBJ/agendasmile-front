b74586f788f31cab0169a2b8c5767e7157d840b2
Augusto <98566110+gutoBJ@users.noreply.github.com>
2026-06-16 23:20:29 -0300

    style: cria responsividade para o sistema

--- sistema-gestao-consultas/src/app/shared/components/header/header.component.ts
+++ sistema-gestao-consultas/src/app/shared/components/header/header.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenav } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  @Input() drawer!: MatSidenav;
+  @Input() mobile = false;
+
   constructor(public auth: AuthService) {}
 }
--- sistema-gestao-consultas/src/app/shared/components/layout/layout.component.ts
+++ sistema-gestao-consultas/src/app/shared/components/layout/layout.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from '../header/header.component';
+
 import { SidebarComponent } from '../sidebar/sidebar.component';
-import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    MatSidenavModule
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {}
+export class LayoutComponent implements OnInit {
+
+  mobile = false;
+
+  ngOnInit(): void {
+    this.verificarTela();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.verificarTela();
+  }
+
+  private verificarTela() {
+    this.mobile = window.innerWidth < 768;
+  }
+}
